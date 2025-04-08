538603d13c25211b365c9a04301fe36034975a63
Shahriar Ahmed <shahriarabiddut@gmail.com>
2025-04-08 19:27:02 +0600

    Session Check on Client!

--- app/(autorized)/layout.tsx
+++ app/(autorized)/layout.tsx
@@ -3,13 +3,19 @@ import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { DashboardShell } from "@/components/dashboard/dashboard-shell";
 import { DashboardSidebar } from "@/components/dashboard/dashboard-sidebar";
 import { ThemeProvider } from "@/components/dashboard/theme-provider";
+import { getServerSession } from "@/lib/action";
+import { redirect } from "next/navigation";
 import type React from "react";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
+  if (!session) {
+    redirect("/sign-in");
+  }
   return (
     <>
       <ThemeProvider>
